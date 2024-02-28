e0acdbcc15b23da5e8aed80e5848f7874d9dd1bc
Yuel Jeiro Daemian Cabarrubia <157999660+DaemianMC@users.noreply.github.com>
2024-02-28 23:23:49 +0800

    Add files via upload

---
+++ InverseLaplace.m
@@ -0,0 +1,59 @@
+clc
+clear
+close all
+
+%% II. Solve for inverse laplace of the following:
+
+syms s t;
+
+%% II.1
+
+D = (8 - 3*s + s^2) / s^3;
+
+d = ilaplace(D);
+
+disp ("d =")
+pretty(d)
+
+
+%% II.2
+
+E = (5 / (s - 2)) - (4*s / (s^2 + 9));
+
+e = ilaplace(E);
+
+disp ("e =")
+pretty(e)
+
+
+%% II.3
+F = 7 / (s^2 + 6);
+
+f = ilaplace(F);
+
+disp ("f =")
+pretty(f)
+
+
+%% 
+G = 1 / (s*(s^2 + 2*s + 2));
+
+g = ilaplace(G);
+
+disp ("g =")
+pretty(g)
+
+H = 5*(s + 2) / (s^2*(s+1)*(s+3));
+
+h = ilaplace(H);
+
+disp ("h =")
+pretty(h)
+
+I = (s^4 + 2*s^3 + 3*s^2 + 4*s + 5) / (s*(s+1));
+
+i = ilaplace(I);
+
+disp ("i =")
+pretty(i)
+
